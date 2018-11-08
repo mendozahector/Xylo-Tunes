0f4efe4fa48abfafba3b6c9fa35f0aa8ae6985cb
Hector Mendoza <hectormendoza@byui.edu>
2018-11-08 13:50:31 -0500

    Buttons modified with transform.scale animations

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -17,7 +17,22 @@ class ViewController: UIViewController{
 
     @IBAction func notePressed(_ sender: UIButton) {
         playSound(sender.tag)
+        sender.showsTouchWhenHighlighted = true
+        pulsate(button: sender)
+    }
+    
+    func pulsate(button: UIButton) {
         
+        let pulse = CASpringAnimation(keyPath: "transform.scale")
+        pulse.duration = 0.6
+        pulse.fromValue = 0.95
+        pulse.toValue = 1.0
+//        pulse.autoreverses = true
+//        pulse.repeatCount = 0
+//        pulse.initialVelocity = 0.5
+//        pulse.damping = 1.0
+        
+        button.layer.add(pulse, forKey: "pulse")
     }
     
     func playSound(_ number: Int) {
@@ -30,5 +45,11 @@ class ViewController: UIViewController{
         }
         
         audioPlayer.play()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 6, options: .allowUserInteraction, animations: {
+            
+        }, completion: nil)
     }
 }
